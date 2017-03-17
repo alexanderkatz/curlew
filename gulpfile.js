5a9ba3b0f875acf451c012c92eeff64b4bbcb082
alexanderkatz <katzkode@gmail.com>
2017-03-16 20:21:09 -0500

    Moved gulp file into theme

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass');
+
+// Create a default task and add the watch task to it
+gulp.task('default', function () {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+//////////////////////////////
+// SASS Task
+//////////////////////////////
+gulp.task('sass', function () {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('static/css/'));
+});
+
+// return gutil.log('Gulp is running!')
